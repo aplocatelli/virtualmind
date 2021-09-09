67ab02fe37dde3623c67013b7ee7708dfd1748c7
Ana Paula Locatelli <aplocatelli@gmail.com>
2021-09-09 16:52:25 -0300

    Update refactor-with-async-await.js
    Updated urls

--- refactor-with-async-await.js
+++ refactor-with-async-await.js
@@ -1,12 +1,10 @@
 var PlayerService = {
     getPlayerTeamId: function(playerId) {
-        //return Promise.resolve($.ajax("/player/" + playerId + "/team")); //ajax already returns a thenable
-        return Promise.resolve($.ajax("https://run.mocky.io/v3/718b969a-526f-4a0b-ba6d-29f347366e0a"));
+        return Promise.resolve($.ajax("/player/" + playerId + "/team")); //ajax already returns a thenable
     },
 
     getPlayers: function(teamId) {
-        //return Promise.resolve($.ajax("/team/" + teamId + "/player")); //ajax already returns a thenable
-        return Promise.resolve($.ajax("https://run.mocky.io/v3/a6490b9d-710c-4145-b6db-4a8f4087a4d5"));
+        return Promise.resolve($.ajax("/team/" + teamId + "/player")); //ajax already returns a thenable
     }
 }
 
