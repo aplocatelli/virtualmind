95038533fdba934458123ffae0ab1e4e9e0c072e
Ana Paula Locatelli <aplocatelli@gmail.com>
2021-09-09 14:20:43 -0300

    Update collections-exercise.js
    Updated with more readable code

--- collections-exercise.js
+++ collections-exercise.js
@@ -1,50 +1,52 @@
 let newItems = [
     {
-      network: 'facebook',
-      text: 'post number 1',
+        network: 'facebook',
+        text: 'post number 1',
     },
     {
-      network: 'twitter',
-      text: 'some twitter text',
+        network: 'twitter',
+        text: 'some twitter text',
     },
     {
-      network: 'gplus',
-      text: 'some gplus stuff',
+        network: 'gplus',
+        text: 'some gplus stuff',
     },
     {
-      network: 'facebook',
-      text: 'post number 2',
+        network: 'facebook',
+        text: 'post number 2',
     },
-  ]
+]
+ 
+//Returns the display name of a social network accordingly to some parameter  
+let getDisplayName = (network) => {
+    switch(network) {
+        case "facebook":
+            return "Facebook";
+        case "twitter":
+            return "Twitter";
+        case "gplus":
+            return "Google +";
+        default:
+            return "";
+    }
+}
   
-  let foo = (arrayOfItems, aNetwork) => {
+let foo = (arrayOfItems, aNetwork) => {
+    //Filter only items whose network property equals to aNetwork and creates a new array
     let filteredArray = arrayOfItems.filter(item => item.network === aNetwork);
     
-    let newArray = filteredArray.map(item => {
-      let newDisplayName;
-      switch(item.network) {
-        case "facebook":
-          newDisplayName = "Facebook";
-          break;
-        case "twitter":
-          newDisplayName = "Twitter";
-          break;
-        case "gplus":
-          newDisplayName = "Google +";
-          break;
-        default:
-          newDisplayName = "";
-      }
-      
-      let newNetworkObj = {
-        displayName: newDisplayName,
-        text: item.text
-      };
-      return newNetworkObj;
+    //Creates a new array from the previously filtered, because the new array has a different property: displayName
+    let newArray = filteredArray.map(item => {      
+        let newNetworkObj = {
+            displayName: getDisplayName(item.network),
+            text: item.text
+        }
+
+        return newNetworkObj;
     });
     
     return newArray;
-  }
+}
   
-  console.log(foo(newItems, 'facebook'));
-  console.log(foo(newItems, 'gplus'));
+console.log(foo(newItems, 'facebook'));
+console.log(foo(newItems, 'gplus'));
