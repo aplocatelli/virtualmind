6afbac52225345cdaeb46121d10d99be34256e8c
Ana Paula Locatelli <aplocatelli@gmail.com>
2021-09-11 07:50:12 -0300

    Update refactor-with-promises.js
    Added more comments

--- refactor-with-promises.js
+++ refactor-with-promises.js
@@ -12,10 +12,10 @@ var PlayerService = {
 var PlayerDetailsController = {
     playerId: 8,
     showTeammatesClick: function() {
-        PlayerService.getPlayerTeamId(this.playerId)
-        .then(team => PlayerService.getPlayers(team.id))
-        .then(playerList => console.log(playerList))
-        .catch(console.log("There was an error."));
+        PlayerService.getPlayerTeamId(this.playerId) //call getPlayerTeamId to find out the team id
+        .then(team => PlayerService.getPlayers(team.id)) //when the promise returned by getPlayerTeamId resolve, then use the team id to call getPlayers 
+        .then(playerList => console.log(playerList)) //when the promise returned by getPlayers resolve, then use the retured playerList to print to the console
+        .catch(console.log("There was an error.")); //if there's an error at any stage
     }
 };
 
