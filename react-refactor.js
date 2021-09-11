ada018808f96f15017ba280c2f17599226c8c569
Ana Paula Locatelli <aplocatelli@gmail.com>
2021-09-11 09:44:51 -0300

    Update react-refactor.js
    Added comments

--- react-refactor.js
+++ react-refactor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+//component with the required lines described by the React Refactor Exercise
 class BigForm extends Component {
   render() {
     const checkboxes = [0, 1, 2];
@@ -11,17 +12,20 @@ class BigForm extends Component {
   }
 }
 
+//component containing the wrapping div, the text showing how many checkboxes are checked, and the 3 checkboxes
 class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: [false, false, false]
+      checked: [false, false, false] //the initial state of the checkboxes
     }
 
-    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this); //handler for when a checkbox is checked/unchecked
   }
 
+  //updates the states array when some checkbox is checked/unchecked
   handleCheckboxChange(checkboxId) {
+    //for each state, verify if it belongs to the changed checkbox and in case it does, toggle its value
     let newCheckedValues = this.state.checked.map((checkboxState, index) => {
       if(index === checkboxId) {
         return !checkboxState;
@@ -29,37 +33,43 @@ class Form extends Component {
       return checkboxState;
     });
 
+    //updates the array of states
     this.setState({
       checked: newCheckedValues
     });
   }
 
+  //returns the amount of checked checkboxes
   getCheckedBoxes() {
     let checkedBoxes = this.state.checked.filter(item => item === true);
     return checkedBoxes.length;
   }
 
+  //displays the amount of checked checkboxes and, for each id in the checkboxes array, creates its own component
   render() {
     return (
       <div className="form">
-        <span>Checked boxes: {this.getCheckedBoxes()}</span>
+        <span>Checked boxes: {this.getCheckedBoxes()}</span> 
         {this.props.checkboxes.map(id => 
-          <Checkbox key={id} id={id} stateValue={this.state.checked[id]} onCheckboxChange={this.handleCheckboxChange}/>)}
+          <Checkbox key={id} id={id} stateValue={this.state.checked[id]} onCheckboxChange={this.handleCheckboxChange}/>)} 
       </div>
     );
   }
 }
 
+//component for each input with type checkbox 
 class Checkbox extends Component {
   constructor(props) {
     super(props);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
   }
 
+  //handler for when a checkbox is checked/unchecked
   handleCheckboxChange(e) {
     this.props.onCheckboxChange(this.props.id);
   }
 
+  //display the checkbox and its label in a span
   render() {
     return (
       <div className="checkbox-wrapper">
